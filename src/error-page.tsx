575403168ee8a3a7e91cafc8a1887846719033ef
Hum Sopha <sopha.humm@gmail.com>
2024-09-22 15:19:42 +0700

    fixed-error-page

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,15 +1,13 @@
-import { useRouteError } from "react-router";
 
-export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
-    return (
-        <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
-    )
+export default function ErrorPage({ error }: { error: { statusText?: string; message?: string } }) {
+  console.error(error);
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{(error.statusText || error.message) as string}</i>
+      </p>
+    </div>
+  )
 }
